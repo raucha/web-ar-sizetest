2b2875e225ae635f944d37ce79a33796e51eaadd
raucha <raucha@users.noreply.github.com>
2022-04-10 18:21:33 +0000

    モデルの表示切替、モデルのblobでの引き渡し、

--- html/main.js
+++ html/main.js
@@ -2,12 +2,19 @@ var app10 = new Vue({
   el: '#app-10',
   vuetify: new Vuetify(),
   data: {
-    qr_size: 0.207, //url
-    // qr_size: 0.057, //hiro
+    // qr_size: 0.207, //url
+    qr_size: 0.057, //hiro
     box_w: 0.1,
     box_d: 0.1,
     box_h: 0.1,
-    count: 4
+    count: 4,
+    file: null,
+    fileData: null,
+    obj_url: "./asset/cube_10cm.obj",
+    obj_scale: 2.5,
+    show_box: true,
+    show_obj: true,
+    show_gltf: true,
   },
   methods: {
     postVal() {
@@ -17,12 +24,63 @@ var app10 = new Vue({
         qr_size: this.qr_size,
         box_w: this.box_w,
         box_d: this.box_d,
-        box_h: this.box_h
+        box_h: this.box_h,
+        obj_url: this.obj_url,
+        obj_scale: this.obj_scale,
+        show_box: this.show_box,
+        show_obj: this.show_obj,
+        show_gltf: this.show_gltf,
       }, '*');
-    }
+    },
+    // ファイル入力が変更された時呼び出されます。
+    onChangeFileInput(selectedFile) {
+      // alert();
+      this.fileData = new Object();
+      // ファイルタイプを設定します。
+      this.fileData.type = selectedFile.type;
+      // ファイル名を設定します。
+      this.fileData.fileName = selectedFile.name;
+
+      // ファイルデータを非同期で読み込みます。
+      this.readFileAsync(selectedFile)
+        .then((result) => {
+          // ファイルデータが読み込めた場合
+          // ファイルデータを設定します。
+          this.fileData.data = result;
+          // alert(this.fileData.type);
+          // alert(this.fileData.fileName);
+          // alert(this.fileData.data);
+        })
+        .then(() => {
+          // var blobObj = new Blob([atob(this.fileData.data)], { type: 'text/plain'});
+          var blobObj = new Blob([this.fileData.data], { type: 'text/plain' });
+          this.obj_url = window.URL.createObjectURL(blobObj);
+
+          // alert(this.obj_url);
+          this.postVal();
+        })
+        .catch((e) => {
+          // エラーの場合
+          alert("error");
+          alert(`e=${e}`);
+          console.log(`e=${e}`);
+        });
+    },
+
+    readFileAsync(file) {
+      return new Promise((resolve, reject) => {
+        // alert("readFileAsync");
+        const reader = new FileReader();
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsArrayBuffer(file);
+      });
+    },
   },
   mounted() {
-    window.setTimeout(this.postVal, 3000);
+    window.setTimeout(this.postVal, 5000);
   },
   beforeDestroy() {
   }
--- html/view.js
+++ html/view.js
@@ -3,29 +3,40 @@ var app = new Vue({
   data: {
     message: 'hellow Vue!',
     box_scale: "1 1 1",
-    box_pos: "0 0.5 0"
+    box_pos: "0 0.5 0",
+    obj_url: "./asset/cube_10cm.obj",
+    obj_scale: "1 1 1",
+    show_box: true,
+    show_obj: true,
+    show_gltf: true,
   },
   methods: {
-    receiveMessage (event) {
+    receiveMessage(event) {
       console.log(event.data);
 
       // 値を生成
-      var scalef = 1/event.data.qr_size;
+      var scalef = 1 / event.data.qr_size;
       var scale_x = event.data.box_d * scalef;
       var scale_y = event.data.box_h * scalef;
       var scale_z = event.data.box_w * scalef;
-      var pos_y = scale_y/2;
-      
+      var pos_y = scale_y / 2;
+
       // 値を設定
-      this.box_scale = {x:scale_x, y:scale_y, z:scale_z};
-      this.box_pos = {x:0, y:pos_y, z:0};
-      this.message = event.data.qr_size;
+      this.box_scale = { x: scale_x, y: scale_y, z: scale_z };
+      this.box_pos = { x: 0, y: pos_y, z: 0 };
+      // this.message = event.data.qr_size;
+      this.message = event.data.obj_url;
+      this.obj_url = event.data.obj_url;
+      this.obj_scale = { x: event.data.obj_scale, y: event.data.obj_scale, z: event.data.obj_scale };
+      this.show_box = event.data.show_box;
+      this.show_obj = event.data.show_obj;
+      this.show_gltf = event.data.show_gltf;
     }
   },
-  mounted () {
+  mounted() {
     window.addEventListener('message', this.receiveMessage)
   },
-  beforeDestroy () {
+  beforeDestroy() {
     window.removeEventListener('message', this.receiveMessage)
   }
 })
