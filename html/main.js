be8cd7ce05dfa9826543cb8f39e77feca12e4e65
raucha <raucha@users.noreply.github.com>
2022-04-10 09:52:05 +0000

    サイズ変更機能追加。 QRが5.7cmでサイズ誤差2mmほど。

--- html/main.js
+++ html/main.js
@@ -2,38 +2,29 @@ var app10 = new Vue({
   el: '#app-10',
   vuetify: new Vuetify(),
   data: {
-    qr_size: 0.1,
-    box_w: 0.5,
-    box_d: 0.5,
-    box_h: 0.5,
+    qr_size: 0.057,
+    box_w: 0.1,
+    box_d: 0.1,
+    box_h: 0.1,
     count: 4
   },
   methods: {
-    // increment() {
-    //   // `this` はコンポーネントインスタンスを参照
-    //   this.count++
-    // }
+    postVal() {
+      // alert("asd")
+      var iframe = document.querySelector('#iframeid');
+      iframe.contentWindow.postMessage({
+        qr_size: this.qr_size,
+        box_w: this.box_w,
+        box_d: this.box_d,
+        box_h: this.box_h
+      }, '*');
+    }
   },
   mounted() {
-    window.addEventListener('message', this.receiveMessage);
-    setInterval(() => {
-      var iframe = document.querySelector('#iframeid');
-      // alert(iframe);
-      iframe.contentWindow.postMessage({
-        qr_size: 10,
-        box_w: 10,
-        box_d: 10,
-        box_h: 10
-      }, '*');
-    }, 5000)
+    window.setTimeout(this.postVal, 3000);
   },
   beforeDestroy() {
-    window.removeEventListener('message', this.receiveMessage)
   }
 })
 
-// alert(app10.count) // => 4
-
-// app10.increment()
-
-// alert(app10.count) // => 5
+// alert("test")
--- html/view.js
+++ html/view.js
@@ -1,12 +1,24 @@
 var app = new Vue({
   el: '#sceneid',
   data: {
-    message: 'hellow Vue!'
+    message: 'hellow Vue!',
+    box_scale: "1 1 1"
   },
   methods: {
     receiveMessage (event) {
       console.log(event.data);
-      this.message = event.data.qr_size
+
+      // 値を生成
+      var scalef = 1/event.data.qr_size;
+      var scale_x = event.data.box_d * scalef;
+      var scale_y = event.data.box_h * scalef;
+      var scale_z = event.data.box_w * scalef;
+      var pos_y = scale_y/2;
+      
+      // 値を設定
+      this.box_scale = {x:scale_x, y:scale_y, z:scale_z};
+      this.box_pos = {x:0, y:pos_y, z:0};
+      this.message = event.data.qr_size;
     }
   },
   mounted () {
@@ -18,3 +30,5 @@ var app = new Vue({
 })
 
 app.message = 'I have changed the data!';
+// app.box_scale = "2 2 2";
+
